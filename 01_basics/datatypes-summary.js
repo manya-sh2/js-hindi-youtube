1d2f018d4fc285ff6d9842fdecf0c6c9dc40162d
manya_sh2 <2005.sharma.manya@gmail.com>
2025-12-03 12:03:32 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,14 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+const bigNumber = 123456787654321n
+
+const heroes = ["shaktiman", "naagraj", 'doga'];
+let myObj = {
+    name: "Manya",
+    age: 20,
+}
+
+const myFunction = function() {
+    console.log("Hello world");
+}
