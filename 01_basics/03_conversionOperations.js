d2309c85fda0de62c11835328eccfd4713b7d5dc
manya_sh2 <2005.sharma.manya@gmail.com>
2025-12-03 11:01:19 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = 33
+
+console.log(typeof score)
+console.log(typeof (score));
+ 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33 (number)
+// "33abc" => NaN (number)
+// true 1; false 0; (number)
+// null 0 (number)
+// undefined NaN (number)
+
+let isLoggedIn = 1
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn);
+
+//1 True; 0 False;
+//"" False; "manya" True;
+
+let no = 33
+let stringNo = String(no)
+console.log(stringNo)
+console.log(typeof stringNo)
