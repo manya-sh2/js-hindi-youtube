64654f5bcaab1c57d80a0c71f8efe4e51b46d354
manya_sh2 <2005.sharma.manya@gmail.com>
2025-12-03 11:25:02 +0000

    operators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = 33
+/*let score = 33
 
 console.log(typeof score)
 console.log(typeof (score));
@@ -23,4 +23,29 @@ console.log(boolIsLoggedIn);
 let no = 33
 let stringNo = String(no)
 console.log(stringNo)
-console.log(typeof stringNo)
+console.log(typeof stringNo)*/
+
+/*************************** operators ***************************/
+let value = 3
+let negValue = -value
+console.log(negValue) //-3
+
+// 2+2 = 4; 2-2 = 0; 2*2 = 4; 2**3 = 8; 2/3 = 0; 2 % 3 = 2;
+
+let str1 = "hello"
+let str2 = "manya"
+let str3 = str1 + str2;
+console.log(str3) //hello manya
+console.log(1 + "2") //12
+console.log("1" + 2) //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+console.log(+true)
+console.log(+"")
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let cnt = 100
+cnt++
+console.log(cnt); //101
+
+
