49a041cb6972bd7f4ffaee915a63562175abfa39
manya_sh2 <2005.sharma.manya@gmail.com>
2025-12-03 11:40:55 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+console.log(undefined>=0); //false
+console.log(undefined==0); //false
+console.log(undefined<0); //false
+
+// === strict check (values awa datatype)
+
+console.log("2" === 2); //false
+
