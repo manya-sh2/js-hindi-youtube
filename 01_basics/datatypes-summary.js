485ee8d37caa72f15697ae5d4f0626f74e625105
manya_sh2 <2005.sharma.manya@gmail.com>
2025-12-04 08:49:27 +0000

    stackAndHeap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,14 +1,31 @@
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id === anotherId)
-const bigNumber = 123456787654321n
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
+// const bigNumber = 123456787654321n
 
-const heroes = ["shaktiman", "naagraj", 'doga'];
-let myObj = {
-    name: "Manya",
-    age: 20,
-}
+// const heroes = ["shaktiman", "naagraj", 'doga'];
+// let myObj = {
+//     name: "Manya",
+//     age: 20,
+// }
 
-const myFunction = function() {
-    console.log("Hello world");
+// const myFunction = function() {
+//     console.log("Hello world");
+// }
+
+// ************************************ stack and heap ************************************ //
+
+let myYTName = "Manyadotcom";
+let anotherName = myYTName;
+anotherName = "chaiaurcode";
+console.log(myYTName);
+console.log(anotherName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl",
 }
+let userTwo = userOne;
+userTwo.email = "manya@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
