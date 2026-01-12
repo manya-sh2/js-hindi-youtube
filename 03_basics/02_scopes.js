a598323e7c01ad2b883df7b587a7f20f1c2a3b65
manya_sh2 <2005.sharma.manya@gmail.com>
2026-01-12 13:42:18 +0000

    scopes2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -16,3 +16,33 @@ if(true){
     console.log("INNER: ", a);
 }
 console.log(a);
+
+function one(){
+    const username = "hitesh";
+    function two(){
+        const website = "youtube";
+        console.log(username);   // hitesh
+    }
+    // console.log(website);  //error
+    two();
+}
+one();  //hitesh
+if(true){
+    const username = "hitesh"
+    if(true){
+        const website = "youtube";
+        console.log(username + website);
+    }
+}
+// console.log(username)   //error
+
+addOne(4)  //5
+function addOne(num){
+    return num + 1;
+}
+
+// addTwo(5)  //error
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(5)
